0059b5c81d5e69a5168e0365af4c2e8ed7e9be24
Sam Rapaport <me@samrapdev.com>
2018-06-16 14:15:28 -0700

    Comment clean up.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,7 +67,8 @@ RSpec.configure do |config|
   # add `FactoryBot` methods
   config.include FactoryBot::Syntax::Methods
 
-  # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
+  # start by truncating all the tables but then use the faster transaction
+  # strategy the rest of the time.
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
     DatabaseCleaner.strategy = :transaction
