dd47b6a783409d9026f8379f25cf0f08feb398e5
Sam Rapaport <me@samrapdev.com>
2018-06-18 21:32:48 -0700

    Build out todos endpoints.

--- spec/factories/todos.rb
+++ spec/factories/todos.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :todo do
-    title { Factory::Lorem.word }
-    created_by { Factory::Number.number(10) }
+    title { Faker::Lorem.word }
+    created_by { Faker::Number.number(10) }
   end
 end
